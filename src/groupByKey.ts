22510a8bf8ae6331625e0c0cccceb433d2836744
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-28 02:59:02 -0300

    Update groupByKey.ts

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,13 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return items.reduce((acc, item) => {
+    const keyValue = item[key] as unknown as string; 
+    if (!acc[keyValue]) {
+      acc[keyValue] = []; 
+    }
+    acc[keyValue].push(item); 
+    return acc;
+  }, {} as GroupsMap<T>);
 }
